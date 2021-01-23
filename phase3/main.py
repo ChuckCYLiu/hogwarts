40779d9d30ee9e0352643658a389984f19dfab2b
arthur <neverf4@163.com>
2021-01-23 19:11:40 +0800

    Update main file

--- phase3/main.py
+++ phase3/main.py
@@ -1,13 +1,7 @@
 from cat import Cat
-from animal import Animal
 
 if __name__ == '__main__':
     my_cat = Cat(name='Angel', age='2', color='white', gender='female', hair='short')
-    # my_cat = Cat('Angel', 'white', 2, 'female', 'short')
     my_cat.cry()
     my_cat.run()
     my_cat.catch_mouse()
-    # my_animal = Animal('Angel', 'white', 2, 'female')
-    # my_animal.cry()
-
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
